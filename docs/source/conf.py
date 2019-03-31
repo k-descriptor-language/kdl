924ea08e12d443e6a6a18010e110fa762595f2f6
Matt Kubej <mkubej@gmail.com>
2019-03-31 18:25:59 -0400

    remove ext for google docstrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,7 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
