f595ce99fc84ab7398ccfb5373428827ba529973
Matt Kubej <mkubej@gmail.com>
2019-04-20 14:06:29 -0400

    Remove flags without implementation in cli

--- kdlc/cli.py
+++ kdlc/cli.py
@@ -22,47 +22,8 @@ logger = logging.getLogger("kdlc.cli")
     help="The input file, either .knwf or .kdl",
     type=click.Path(exists=True),
 )
-@click.option(
-    "--modify",
-    "-m",
-    "modify_file",
-    help="The KNIME workflow file (.knwf) being modified",
-    type=click.Path(exists=True),
-)
-@click.option(
-    "--nodes",
-    "-n",
-    help="The path to the custom node templates",
-    type=click.Path(exists=True),
-)
-def prompt(input_file: str, output_file: str, modify_file: str, nodes: str) -> None:
-    if modify_file and nodes:
-        raise click.UsageError("nodes and modify options cannot be used simultaneously")
-
-    if modify_file:
-        if Path(modify_file).suffix != ".knwf":
-            raise click.BadParameter("modify file must be of file type *.knwf")
-
-        if Path(input_file).suffix != ".kdl":
-            raise click.BadParameter("input file must be of file type *.kdl")
-
-        if Path(output_file).suffix != ".knwf":
-            raise click.BadParameter("output file must be of file type *.knwf")
-
-        kdlc.update_workflow_with_kdl(input_file, output_file, modify_file)
-
-    elif nodes:
-        if Path(input_file).suffix == ".kdl" and Path(output_file).suffix == ".knwf":
-            kdlc.kdl_to_workflow_custom_template(input_file, output_file, nodes)
-        elif Path(input_file).suffix == ".knwf" and Path(output_file).suffix == ".kdl":
-            kdlc.workflow_to_kdl_custom_template(input_file, output_file, nodes)
-        else:
-            raise click.BadParameter(
-                "Input/output file type mismatch. Either .kdl --> .knwf or "
-                ".knwf --> .kdl"
-            )
-
-    elif input_file and output_file:
+def prompt(input_file: str, output_file: str) -> None:
+    if input_file and output_file:
         if Path(input_file).suffix == ".kdl" and Path(output_file).suffix == ".knwf":
             kdlc.kdl_to_workflow(input_file, output_file)
         elif Path(input_file).suffix == ".knwf" and Path(output_file).suffix == ".kdl":
