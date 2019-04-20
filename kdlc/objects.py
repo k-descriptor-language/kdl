f3268fedcd9383e349153bd9a8f75a25fd04e640
Jared Faucher <jared.faucher@gmail.com>
2019-04-20 18:20:56 -0400

    check that model/variables are not empty when merging

--- kdlc/objects.py
+++ kdlc/objects.py
@@ -95,7 +95,8 @@ class Node(AbstractNode):
         Merges workflow variables into Node's model
 
         """
-        self.__merge_variables_helper(self.model, self.variables)
+        if self.model and self.variables:
+            self.__merge_variables_helper(self.model, self.variables)
 
     def __merge_variables_helper(self, model_list: list, var_list: list) -> None:
         """
