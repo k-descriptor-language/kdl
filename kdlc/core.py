fba3d0126d2c7714d6bcafe9b5954f106c50bf8e
Jared Faucher <jared.faucher@gmail.com>
2019-05-04 01:51:55 -0400

    isInstance

--- kdlc/core.py
+++ kdlc/core.py
@@ -461,9 +461,9 @@ def normalize_connections(
         else:
             connection.dest_node = node_dict[connection.dest_id]
 
-        if type(connection.source_node) is MetaNode:
+        if isinstance(connection.source_node, MetaNode):
             connection.source_port = str(int(connection.source_port) - 1)
-        if type(connection.dest_node) is MetaNode:
+        if isinstance(connection.dest_node, MetaNode):
             connection.dest_port = str(int(connection.dest_port) - 1)
 
     metanodes = [
